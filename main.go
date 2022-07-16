01abab36ad186fedb3e29580848bce369d27f3c1
Narapat <77012514+topnarapat@users.noreply.github.com>
2022-07-16 22:56:34 +0700

    Answer

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	var whatIsIt string
+	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+	sd, _ := base64.StdEncoding.DecodeString(secret)
+
+  for _, v := range sd {
+    whatIsIt = string(v) + whatIsIt
+  }
+
+	fmt.Println(whatIsIt)
+}
